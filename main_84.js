29f6bf57f37495a2661e56907592e8211b0a9b6a
hogwartshogwartshoggyhoggyhogwarts <102550637+hogwartshogwartshoggyhoggyhogwarts@users.noreply.github.com>
2022-07-04 16:23:45 +0100

    Add files via upload

---
+++ main_84.js
@@ -0,0 +1,15 @@
+pizza_list_array = [];
+
+function getmenu()
+	{    
+        // Give the appropriate id name as pizza_name
+        var item = document.getElementById(" pizza_name       ").value; 
+       
+        //Add push() function to push item into array
+        pizza_list_array. push     (item);
+        
+        var remove_commas = pizza_list_array.join("<br># ");
+        document.getElementById("display_menu").innerHTML =  "# " + remove_commas;}
+        var item = document.getElementById("display_menu").innerHTML=htmldata; 
+        var item = document.getElementById("add_suggestion").value;
+        menu_list_array.push (item);
